2b5040eacafe997e647d3f351f05c5abf1f82ffd
Jose Alejandro Matute <jose@jose-laptop.(none)>
2010-11-07 15:43:40 -0600

    esqueleto con rails

--- config.ru
+++
@@ -1,4 +0,0 @@
-require 'correr'
-
-run Sinatra::Application
-
---
+++ tabEditor/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TabEditor::Application.config.secret_token = 'fa78714964042f10da58e2670279a9b35c9a1c062302009bd8e0b716b4d48378060380f2b7c04735089e44e19771fa65832088fd5352f61570e526bafd0072a0'
