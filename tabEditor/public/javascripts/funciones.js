e3f9ba064c02a403be6e8036a4098974a20a4160
Jose Alejandro Matute <jose@jose-laptop.(none)>
2010-12-13 19:39:52 -0600

    cambios buenos

--- tabEditor/public/javascripts/funciones.js
+++ tabEditor/public/javascripts/funciones.js
@@ -4,16 +4,17 @@ var mapa = {"do1":"3/5","domas":"1/2","re":"5/5","mi":"2/4","fa":"3/4","sol":"5/
 
 var tocar = function(ruta){
 	var snd = new Audio("sounds/" + ruta+ ".mp3");
-	$("#tab_body").append("notes " +mapa[ruta]);
-	$(".vex-tabdiv").tabdiv("renderText",$(".vex-tabdiv").tabdiv("code") + mapa[ruta]);	
+	$("#tab_body").append(mapa[ruta] + " ");
+	$(".vex-tabdiv").tabdiv("renderText",$(".vex-tabdiv").tabdiv("code") + mapa[ruta] + " ");	
 	/*$(".vex-tabdiv").append(nombre);*/
 	snd.play();
 }
 
 $(function(){
 
+	/*$(".vex-tabdiv").tabdiv();
+	$(".vex-tabdiv").tabdiv("renderText", "notes");*/
 	$(".vex-tabdiv").tabdiv();
-	$(".vex-tabdiv").tabdiv("renderText", "notes");
 	$(".blanche").click(function(event)
 	{	var nombre = $(this)[0].id;
 		tocar(nombre);
